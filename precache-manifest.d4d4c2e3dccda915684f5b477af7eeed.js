f09dcfb1bcc421b72acaaf7695f9b0cdf6e372af
Davekoder <davekoder@gmail.com>
2020-07-17 13:23:40 +0800

    Updates

---
+++ precache-manifest.d4d4c2e3dccda915684f5b477af7eeed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd12272e6fd91aaf77cad7bdc8e8d5a",
+    "url": "/decide-for-me/index.html"
+  },
+  {
+    "revision": "07b6ee10615b9bce3a8a",
+    "url": "/decide-for-me/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "1e80b8d24ceaa3b23a9c",
+    "url": "/decide-for-me/static/css/main.45dcb506.chunk.css"
+  },
+  {
+    "revision": "07b6ee10615b9bce3a8a",
+    "url": "/decide-for-me/static/js/2.169dc0b7.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/decide-for-me/static/js/2.169dc0b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e80b8d24ceaa3b23a9c",
+    "url": "/decide-for-me/static/js/main.414ddf86.chunk.js"
+  },
+  {
+    "revision": "c1fa45d241fe48ed214a",
+    "url": "/decide-for-me/static/js/runtime-main.edb4b505.js"
+  }
+]);
